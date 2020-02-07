d9f9a516d04bee9d1320a69a0471766d3e14ba42
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-07 13:52:16 -0800

    Add displayData() to display object data to DOM

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
+import { GalacticAge } from "./GalacticAge.js";
 
+function displayData(GalacticAge) {
+  $("#mercuryAge").html(GalacticAge.mercuryAge);
+  $("#earthAge").html(GalacticAge.earthAge);
+  $("#marsAge").html(GalacticAge.marsAge);
+  $("#jupiterAge").html(GalacticAge.jupiterAge);
+}
 
 $(document).ready(function() {
-
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    let age = $("#age").val();
+    // let faveThing = $("#faveThing");
+    // let faveGenre = $("#faveGenre");
+    // let isAlien = $("input:radio[name=isAlien]:checked");
+    let galacticAge = new GalacticAge(age);
+    galacticAge.calculateYearsLeft();
+    displayData(galacticAge);
+  });
 });

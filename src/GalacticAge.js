3aad92fcdca25fc8b52639621fdefad0082ef4f4
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-07 12:02:56 -0800

    Pass test for venusYearsCheatedDeath

--- src/GalacticAge.js
+++ src/GalacticAge.js
@@ -11,6 +11,7 @@ export class GalacticAge {
     if(this.earthAge >= 150) {
       this.earthYearsCheatedDeath = this.earthAge - 150;
       this.mercuryYearsCheatedDeath = Math.floor(this.earthYearsCheatedDeath / 0.24);
+      this.venusYearsCheatedDeath = Math.floor(this.earthYearsCheatedDeath / 0.62);
     }
   }
   calculateLifeChoices(favoriteThing, favoriteGenre, isAlien) {

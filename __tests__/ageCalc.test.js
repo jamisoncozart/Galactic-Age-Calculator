3a25434447ff77a94797f81711a4b228d34f6694
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-07 09:28:33 -0800

    Test for accurate creation and calculation of mercuryAge value

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -5,4 +5,8 @@ describe('GalacticAge Constructor', () => {
     let gAge = new GalacticAge(23);
     expect(gAge.earthAge).toEqual(23);
   })
+  test('should check for correctly calculated mercuryAge', () => {
+    let gAge = new GalacticAge(23);
+    expect(gAge.mercuryAge).toEqual(95);
+  })
 })
